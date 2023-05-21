84bde188b11d6bb433ff8d42bdf166ed17aa96e3
olehpetrovasyliev <olegvasiilyevv@gmail.com>
2023-05-21 21:29:47 +0300

    first steps

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,14 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const gallery = document.querySelector(".gallery");
+const galleryHTML = galleryItems.map();
+
+const onImgClick = (evt) => {
+  if (evt.target.node !== "IMG") {
+    return;
+  }
+};
+
+gallery.addEventListener("click", onImgClick);
 
 console.log(galleryItems);
