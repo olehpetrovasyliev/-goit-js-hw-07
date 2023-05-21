a36f467fdb907f52b6252c982064c552ea3ae637
olehpetrovasyliev <olegvasiilyevv@gmail.com>
2023-05-21 21:51:43 +0300

    adds gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,12 +1,20 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 const gallery = document.querySelector(".gallery");
-const galleryHTML = galleryItems.map();
+const galleryHTML = galleryItems
+
+  .map(
+    (img) =>
+      `<li><img src = ${img.preview} alt = ${img.description} class = "gallery__image"></li> `
+  )
+  .join("");
+gallery.insertAdjacentHTML("afterbegin", galleryHTML);
 
 const onImgClick = (evt) => {
-  if (evt.target.node !== "IMG") {
+  if (evt.target.nodeName !== "IMG") {
     return;
   }
+  //   console.log(evt.target.nodeName);
 };
 
 gallery.addEventListener("click", onImgClick);
