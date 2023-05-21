809423f32e3fe264e5cc59ebd173ac23be7dc119
olehpetrovasyliev <olegvasiilyevv@gmail.com>
2023-05-22 00:33:42 +0300

    updates task1 but the libray is not connecting

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,20 +1,26 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+
 const gallery = document.querySelector(".gallery");
 const galleryHTML = galleryItems
-
   .map(
     (img) =>
-      `<li><img src = ${img.preview} alt = ${img.description} class = "gallery__image"></li> `
+      `<li class = "gallery__item"><a class="gallery__link" href = ${img.original}><img src = ${img.preview} alt = ${img.description} data-source = ${img.original} class = "gallery__image"></a></li>`
   )
   .join("");
 gallery.insertAdjacentHTML("afterbegin", galleryHTML);
 
 const onImgClick = (evt) => {
+  evt.preventDefault();
   if (evt.target.nodeName !== "IMG") {
     return;
   }
-  //   console.log(evt.target.nodeName);
+  const basicLightbox = require("basiclightbox");
+  const instance = basicLightbox.create(
+    `<img src = ${evt.target.dataset.source} alt = ${evt.target.alt} width = "800" height = "600">`
+  );
+
+  instance.show();
 };
 
 gallery.addEventListener("click", onImgClick);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,22 +1,4 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-const gallery = document.querySelector(".gallery");
-const galleryHTML = galleryItems
-
-  .map(
-    (img) =>
-      `<li><img src = ${img.preview} alt = ${img.description} class = "gallery__image"></li> `
-  )
-  .join("");
-gallery.insertAdjacentHTML("afterbegin", galleryHTML);
-
-const onImgClick = (evt) => {
-  if (evt.target.nodeName !== "IMG") {
-    return;
-  }
-  //   console.log(evt.target.nodeName);
-};
-
-gallery.addEventListener("click", onImgClick);
 
 console.log(galleryItems);
