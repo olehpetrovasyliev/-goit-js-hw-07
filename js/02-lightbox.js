e372d4736a5feb9f2090549dcefcce5b469f33e6
olehpetrovasyliev <olegvasiilyevv@gmail.com>
2023-05-22 15:32:50 +0300

    deletes comments and adds full alt to img

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -4,7 +4,7 @@ const gallery = document.querySelector(".gallery");
 const galleryHTML = galleryItems
   .map(
     (img) =>
-      `<li class = "gallery__item"><a class="gallery__link" href = ${img.original}><img src = ${img.preview} alt = ${img.description}  class = "gallery__image"></a></li>`
+      `<li class = "gallery__item"><a class="gallery__link" href = ${img.original}><img src = ${img.preview} alt = "${img.description}"  class = "gallery__image"></a></li>`
   )
   .join("");
 gallery.insertAdjacentHTML("afterbegin", galleryHTML);
@@ -14,21 +14,3 @@ new SimpleLightbox(".gallery a", {
   captionsData: "alt",
   captionDelay: 250,
 });
-
-/*galleryModal.on("show.simplelightbox", function () {
-  console.log("Hello");
-});*/
-
-/*const onImgClick = (evt) => {
-  evt.preventDefault();
-  if (evt.target.nodeName !== "IMG") {
-    return;
-  }
-  // console.log(evt.target.nodeName);
-  const galleryModal = new SimpleLightbox(".gallery a");
-  galleryModal.on("show.simplelightbox", function () {
-    console.log("Hello");
-  });
-};
-
-gallery.addEventListener("click", onImgClick);*/
